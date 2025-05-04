d8172d4784415b3b8de9c3598a9ee65dacccff80
Safiro <altxafiro@gmail.com>
2025-05-04 17:37:12 +0300

    Update date

--- src/consts/dates.ts
+++ src/consts/dates.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon';
 
 export const ReleaseDate = {
-  GTA6: DateTime.fromISO('2025-09-01T12:00:00.000'),
+  GTA6: DateTime.fromISO('2026-05-26T12:00:00.000'),
 } as const;
